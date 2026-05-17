bb6667beb11c72e604241de4cc2e38107b72c794
dspaan <d.spaan@gmail.com>
2026-05-17 13:29:54 +0200

    Update check_vicidial_live_agents.sh
    Need to use another database user.

--- scripts/check_vicidial_live_agents.sh
+++ scripts/check_vicidial_live_agents.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-vicidial_live_agents=$(mysql asterisk -N -s -e 'select count(*) from vicidial_live_agents')
-        echo "Aantal live agents: ${vicidial_live_agents}"
-        exit 0
+vicidial_live_agents=$(mysql -u cron -p1234 asterisk -N -s -e 'select count(*) from vicidial_live_agents')
+
+echo "Aantal live agents: ${vicidial_live_agents} | live_agents=${vicidial_live_agents};;;0;"
+
+exit 0
