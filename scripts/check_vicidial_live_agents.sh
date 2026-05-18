c46a5abec7cec6fb77f068e0a76b31ac3842d560
dspaan <d.spaan@gmail.com>
2026-05-18 21:00:01 +0200

    Update check_vicidial_live_agents.sh
    exclude remote agents

--- scripts/check_vicidial_live_agents.sh
+++ scripts/check_vicidial_live_agents.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-vicidial_live_agents=$(mysql -u cron -p1234 asterisk -N -s -e 'select count(*) from vicidial_live_agents')
+vicidial_live_agents=$(mysql -u cron -p1234 asterisk -N -s -e "
+SELECT COUNT(*)
+FROM vicidial_live_agents
+WHERE extension IS NULL
+   OR extension NOT LIKE 'R/%';
+")
 
 echo "Aantal live agents: ${vicidial_live_agents} | live_agents=${vicidial_live_agents};;;0;"
 
